b0658193506e22e400eda30b8a9fc2893ef49bec
Belgarus <Club.Mate@tutamail.com>
2025-11-27 13:47:20 +0100

    added the basic structure

---
+++ target/debug/chess_turnament_manager.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/10145000/Documents/chess_turnament_manager/target/debug/chess_turnament_manager: /mnt/c/Users/10145000/Documents/chess_turnament_manager/src/main.rs
---
+++ target/debug/deps/chess_turnament_manager-72f2328f623bf6c3.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/10145000/Documents/chess_turnament_manager/target/debug/deps/chess_turnament_manager-72f2328f623bf6c3.d: src/main.rs
+
+/mnt/c/Users/10145000/Documents/chess_turnament_manager/target/debug/deps/chess_turnament_manager-72f2328f623bf6c3: src/main.rs
+
+src/main.rs:
